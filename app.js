4e1e6f454afe7febd9de3da967c4642f1df611af
ShashankBiplav <Biplavshashank7@gmail.com>
2020-07-14 22:45:12 +0530

    Added routes and controller for GET and POST request for a 'post'

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const feedRoutes = require('./routes/feed.js');
 
 const app = express();
+
+app.use(bodyParser.json()); // for-> application/json
+
+app.use('/feed',feedRoutes);
 
 app.listen(8080);
