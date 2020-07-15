9e34b8523337746255b2359e0e45fae0bd7ad6f5
ShashankBiplav <Biplavshashank7@gmail.com>
2020-07-15 16:50:30 +0530

    Defined the data model for a single post

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,15 @@
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-        posts: [{title: 'First Post', content: 'This is my first post !'}]
+        posts: [{
+            _id: 1,
+            title: 'First Post',
+            content: 'This is my first post !',
+            imageUrl: 'images/car.jpg',
+            creator: {
+                name:'Shashank Biplav'
+            },
+            createdAt: new Date(),
+        }]
     });
 };
 
@@ -10,6 +19,10 @@ exports.createPost = (req, res, next) => {
     //TODO: create post in DB
     res.status(201).json({
         message: 'Post created successfully',
-        post: {id: new Date().toISOString(), title: title, content: content}
+        post: {
+            id: new Date().toISOString(),
+            title: title,
+            content: content
+        }
     });
 };
