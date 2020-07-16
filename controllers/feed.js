211c19f4111c54bb50d9df73f4a22d1f2f9eaf87
ShashankBiplav <Biplavshashank7@gmail.com>
2020-07-17 03:09:39 +0530

    Connection successful to DB, dummy POST request sent

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,7 @@
 const expressValidator = require('express-validator');
 
+const Post = require('../models/post.js');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [{
@@ -8,7 +10,7 @@ exports.getPosts = (req, res, next) => {
             content: 'This is my first post !',
             imageUrl: 'images/car.jpg',
             creator: {
-                name:'Shashank Biplav'
+                name: 'Shashank Biplav'
             },
             createdAt: new Date(),
         }]
@@ -19,23 +21,30 @@ exports.createPost = (req, res, next) => {
     const errors = expressValidator.validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(422).json({
-            message:'Validation failed, entered data is not valid',
+            message: 'Validation failed, entered data is not valid',
             errors: errors.array()
         });
     }
     const title = req.body.title;
     const content = req.body.content;
     //TODO: create post in DB
-    res.status(201).json({
-        message: 'Post created successfully',
-        post: {
-            _id: new Date().toISOString(),
-            title: title,
-            content: content,
-            creator:{
-                name: 'Shashank Biplav'
-            },
-            createdAt: new Date()
-        }
+    const post = new Post({
+        title: title,
+        content: content,
+        imageUrl: 'images/car.jpg',
+        creator: {
+            name: 'Shashank Biplav'
+        },
+    });
+    post.save()
+    .then(result=>{
+        console.log(result);
+        res.status(201).json({
+            message: 'Post created successfully',
+            post: result
+        });
+    })
+    .catch((err) => {
+        console.log(err);
     });
 };
