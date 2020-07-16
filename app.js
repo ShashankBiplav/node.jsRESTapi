614c313918a912155e011de8713f0ae2271be02c
ShashankBiplav <Biplavshashank7@gmail.com>
2020-07-17 04:12:03 +0530

    Used multer to create path and upload images to DB

--- app.js
+++ app.js
@@ -6,41 +6,68 @@ const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
 
+const multer = require('multer');
+
 require('dotenv').config();
 
 const feedRoutes = require('./routes/feed.js');
 
-const app = express();
 const port = process.env.PORT || 8080;
 
+const app = express();
+
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + '-' + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
 app.use(bodyParser.json()); // for-> application/json
+
+app.use(multer({
+    storage: fileStorage,
+    fileFilter: fileFilter
+}).single('image'));
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 
-app.use('/feed',feedRoutes);
+app.use('/feed', feedRoutes);
 
 app.use((error, req, res, next) => {
     console.log(error);
-    const status= error.statusCode || 500;
-    const message= error.message;
-    res.status(status).json({message: message});
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({
+        message: message
+    });
 });
 
-mongoose.connect(process.env.MONGODB_URI,{
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-})
-.then(result=>{
-    console.log('connection successful');
-    app.listen(port, ()=>{
-        console.log(`Listening on port ${port}`);
-    });
-})
-.catch(err=>console.log(err));
+mongoose.connect(process.env.MONGODB_URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    })
+    .then(result => {
+        console.log('connection successful');
+        app.listen(port, () => {
+            console.log(`Listening on port ${port}`);
+        });
+    })
+    .catch(err => console.log(err));
