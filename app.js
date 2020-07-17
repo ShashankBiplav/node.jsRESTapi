b8fa5456fd2bed6bc3b4f1126173e8edacc86cc7
ShashankBiplav <Biplavshashank7@gmail.com>
2020-07-18 04:24:05 +0530

    Added the delete post route and controller

--- app.js
+++ app.js
@@ -62,7 +62,8 @@ app.use((error, req, res, next) => {
 
 mongoose.connect(process.env.MONGODB_URI, {
         useUnifiedTopology: true,
-        useNewUrlParser: true
+        useNewUrlParser: true,
+        useFindAndModify: false
     })
     .then(result => {
         console.log('connection successful');
--- routes/feed.js
+++ routes/feed.js
@@ -21,4 +21,6 @@ router.put('/post/:postId', [
     expressValidator.check('content').trim().isLength({min:5})
 ], feedController.updatePost);
 
+router.delete('/post/:postId',feedController.deletePost);
+
 module.exports = router;
