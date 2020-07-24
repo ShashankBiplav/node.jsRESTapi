e797872e46116471d535d75d7e077103b3fd3ec2
Shashank Biplav <Biplavshashank7@gmail.com>
2020-07-24 21:53:47 +0530

    Added 'sinon' as dev dependency

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,5 +1,7 @@
 const expect = require('chai').expect;
 
+const jwt = require('jsonwebtoken');
+
 const authMiddleware = require('../middleware/is-auth.js');
 
 describe('auth middleware', function()  {
@@ -19,5 +21,27 @@ describe('auth middleware', function()  {
             }
         };
         expect(authMiddleware.bind(this, req, {}, ()=>{})).to.throw();
+    });
+
+    it('should throw an error if the token cannot be verified', function(){
+        const req = {
+            get: function(headerName){
+                return 'bearer xyzabc';
+            }
+        };
+        expect(authMiddleware.bind(this, req, {}, ()=>{})).to.throw();
+    });
+
+    it('should yield a userId after decoding the token', function(){
+        const req = {
+            get: function(headerName){
+                return 'bearer xyzabc';
+            }
+        };
+        jwt.verify = function(){
+            return {userId: 'abc'};
+        };
+        authMiddleware(req, {}, ()=>{});
+        expect(req).to.have.property('userId');
     });
 });
